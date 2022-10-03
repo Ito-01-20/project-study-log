57b392234402060b8f0aea78ca7c58f161dfb46e
Ito-01-20 <kaiki_2001_hebi@icloud.com>
2022-10-03 17:39:03 +0900

    add blogs

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,17 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from blog.models import Article, models
 
-def test(request):
-    return HttpResponse('test page')
+def index(request):
+    objs = Article.objects.all()
+    context = {
+        'articles': objs,
+    }
+    return render(request, 'blog/blogs.html', context)
+
+def article(request, pk):
+    obj = Article.objects.get(pk=pk)
+    context = {
+        'article': obj
+    }
+    return render(request, 'blog/article.html', context)
