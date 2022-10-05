d8a4d5caf30677b305e1939a263eef4d0a55f847
Ito-01-20 <kaiki_2001_hebi@icloud.com>
2022-10-05 21:49:39 +0900

    add invalid

--- mysite/views.py
+++ mysite/views.py
@@ -16,6 +16,14 @@ def index(request):
 class Login(LoginView):
     template_name = 'mysite/auth.html'
     
+    def form_valid(self, form):
+        messages.success(self.request, 'ログイン完了!')
+        return super().form_valid(form)
+    
+    def form_invalid(self, form):
+        messages.error(self.request, 'エラー!')
+        return super().form_invalid(form)
+    
 def signup(request):
     context = {}
     if request.method == 'POST':
